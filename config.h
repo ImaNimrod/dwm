38455ab2a9ea4201b412ded263de04a0c587a45f
nimrod <nun.ya@business.com>
2023-06-26 16:28:43 -0400

    config: updated dmenucmd and default gap size

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 7;        /* gaps between windows */
+static const unsigned int gappx     = 9;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -56,8 +56,9 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_highlight, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0";      /* component of dmenucmd, manipulated in spawn() */
+static char dmenulines[3] = "15";   /* component of dmenucmd, the number of menu options dmenu displays */
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", dmenulines, "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_highlight, "-sf", col_gray4, NULL };
 
 #include <X11/XF86keysym.h>
 
