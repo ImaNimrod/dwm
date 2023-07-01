0bf40bc0d0cf66ffcae5fe964dad5f334cd052a6
nimrod <nun.ya@business.com>
2023-07-01 17:25:54 -0400

    made qemu float by default

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki:size=16:autohint=true:antialias=true", "JoyPixels:pixelsize=11" };
-static const char dmenufont[]       = "mononoki:size=16:autohint=true:antialias=true";
+static const char *fonts[]          = { "mononoki:size=14:autohint=true:antialias=true", "JoyPixels:pixelsize=11" };
+static const char dmenufont[]       = "mononoki:size=14:autohint=true:antialias=true";
 static const char col_gray1[]       = "#262731";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ffffff";
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "qemu",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
